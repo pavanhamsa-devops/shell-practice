277b306801e5217688e44808c2fb15edc52ff797
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-04 07:24:14 +0530

    action

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh: Hi Ramesh, How are you?"
+echo "Ramesh: Hello Suresh, am good. what about you?"
+echo "Suresh: How is going on your work?"
+echo "Ramesh: Its as usual only Suresh"
