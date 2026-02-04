8199e5d1fae0b603c48313aada2c67c4b6360679
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-04 08:35:49 +0530

    action

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
     echo "Given number:$NUMBER is greater then 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "Given number:$NUMBER is equals to 20"
 else
     echo "Given number:$NUMBER is lesser than 20"
 fi
