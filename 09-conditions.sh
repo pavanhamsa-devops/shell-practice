dce3cd829d825ccf74e7d35de6c0c2932a932d24
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-04 08:33:51 +0530

    action

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
-    echo "Given number: is greater then 20"
+    echo "Given number:$NUMBER is greater then 20"
 else
-    echo "Given number: is lesser than 20"
+    echo "Given number:$NUMBER is lesser than 20"
 fi
