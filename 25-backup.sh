5a11771b82f7c55a1fb124d511824180f4b57f15
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-08 15:05:05 +0530

    installing mysql too

--- 25-backup.sh
+++ 25-backup.sh
@@ -5,10 +5,13 @@ G="\e[32m"
 Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/backup.log"
+DAYS=${3:-14}
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N"
@@ -18,10 +21,20 @@ fi
 mkdir -p $LOGS_FOLDER
 
 USAGE(){
-    echo -e "sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[Default 14days]"
+    echo -e "$R USAGE:: udo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[Default 14days] $N"
     exit 1
 }
 
 if [ $# -lt 2 ]; then
     USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo -e "$R $SOURCE_DIR does not exist $N"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+    echo -e "$R $DEST_DIR does not exist $N"
+    exit 1
 fi
