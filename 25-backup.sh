db4ae0b273cd00fd0f0765af3e8ceda1d7177f78
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-08 15:38:40 +0530

    installing mysql too

--- 25-backup.sh
+++ 25-backup.sh
@@ -51,7 +51,7 @@ log "Destination Directory: $DEST_DIR"
 log "Days: $DAYS"
 
 #To check if variable is empty or not
-if [ -z $FILES ]; then
+if [ ! -z $FILES ]; then
     log "no files to archive"
 else
     #app-logs-$timestamp.zip - format of zip file
