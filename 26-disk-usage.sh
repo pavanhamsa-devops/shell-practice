a3f05edd280ee5d7977bced1f2d33249cb7f8350
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-08 17:13:48 +0530

    installing mysql too

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -5,21 +5,23 @@ G="\e[32m"
 Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
-SOURCE_DIR=$1
-DEST_DIR=$2
-
-USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="/var/log/shell-script/backup.log"
-DAYS=${3:-14}
-
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script with root user access $N"
-    exit 1
-fi
-
-mkdir -p $LOGS_FOLDER
+MESSAGE=""
 
 log(){
-    echo -e "$(date "+%Y-%m-%d %H:%M:%s") | $1" | tee -a $LOGS_FILE
+    echo -e "$(date "+%Y-%m-%d %H:%M:%s") | $1"
 }
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+USAGE_THRESHOLD=5
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+
+        if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+            MESSAGE+="High disk usage on $PARTITION:$USAGE"
+
+done <<< $DISK_USAGE
+
+echo -e "$MESSAGE"
