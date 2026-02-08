a2eac97b9d43fed75566204c77ce5645ce645339
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-08 15:08:30 +0530

    installing mysql too

--- 25-backup.sh
+++ 25-backup.sh
@@ -30,11 +30,11 @@ if [ $# -lt 2 ]; then
 fi
 
 if [ ! -d $SOURCE_DIR ]; then
-    echo -e "$R $SOURCE_DIR does not exist $N"
+    echo -e "$R Source directory: $SOURCE_DIR does not exist $N"
     exit 1
 fi
 
 if [ ! -d $DEST_DIR ]; then
-    echo -e "$R $DEST_DIR does not exist $N"
+    echo -e "$R Destination directory: $DEST_DIR does not exist $N"
     exit 1
 fi
